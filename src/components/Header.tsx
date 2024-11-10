42bc7784284d00ef79e2da17f626159fbc30be12
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-10 11:11:17 +0500

    header part

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from  '../assets/icons/Logo.png';
+const Header = () =>{
+return(
+    <header className="header">
+        <h1><img src={logo} alt="" /> YA BAO</h1>
+        <nav>
+        <a href="/">Паста</a>
+        <a href="/">Супы</a>
+        <a href="/">Салаты</a>
+        <a href="/">Антипасти</a>
+        <a href="/">Напитки</a>
+        <a href="/">Десерты</a>
+        <a href="/">Бакалея</a>
+        <a href="/">Акции</a>
+        <a href="/">Комбо</a>
+        <a href="/">Контакты</a>
+        <a href="/bonuses">Бонусы</a>
+        <a href="/basket">Корзина</a>
+        </nav>
+    </header>
+);
+};
