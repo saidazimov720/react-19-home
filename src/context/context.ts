69de9dc78f2af6c2846984e2c7782eb112e973d8
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-11 19:05:52 +0500

    Context

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,1 +1,14 @@
 import { createContext } from "react";
+import {Product} from '../components/ProductCard';
+
+type ContextData = {
+    basket: Product[];
+    addItemToBasket: ( product:Product) => void;
+}
+
+const Context = createContext<ContextData>((
+    basket:[],
+    addItemToBasket: () => {},
+));
+
+export default Context;
