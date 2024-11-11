9d17107ca5c84e74467817f48adadb12482e210c
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-11 14:34:01 +0500

    add the new part

--- src/components/PromotionSection.tsx
+++ src/components/PromotionSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid, Card, CardContent, Typography } from '@mui/material';
-import sweet from '../assets/images/image 26.png';
+import sweet from '../assets/images/promotion.png';
 
 const promotions = [
     { id: 1, title: 'Торты на заказ', image: {sweet} },
@@ -10,10 +10,16 @@ const PromotionsSection: React.FC = () => {
     return (
         <Grid container spacing={3}>
             {promotions.map((promo) => (
-                <Grid item xs={12} sm={6} md={4} key={promo.id}>
+                <Grid item xs={12} sm={6} md={6} key={promo.id}>
                     <Card>
                         <CardContent>
                             <Typography variant="h5">{promo.title}</Typography>
+                            <img src={sweet} alt="promotion" />
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                          <img src={sweet} alt="" />  
                         </CardContent>
                     </Card>
                 </Grid>
