fabb2d8b38c277b793b85a16a0a3cfcb87d98f28
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-11 14:51:08 +0500

    make little gallery

--- src/components/PromotionSection.tsx
+++ src/components/PromotionSection.tsx
@@ -10,16 +10,12 @@ const PromotionsSection: React.FC = () => {
     return (
         <Grid container spacing={3}>
             {promotions.map((promo) => (
-                <Grid item xs={12} sm={6} md={6} key={promo.id}>
+                <Grid item xs={12} sm={6} md={12} key={promo.id} justifyContent={'space-between'}>
                     <Card>
                         <CardContent>
                             <Typography variant="h5">{promo.title}</Typography>
                             <img src={sweet} alt="promotion" />
-                        </CardContent>
-                    </Card>
-                    <Card>
-                        <CardContent>
-                          <img src={sweet} alt="" />  
+                            <img src={sweet} alt="promotion" />
                         </CardContent>
                     </Card>
                 </Grid>
